2b1457250314f64aa4fe4ee7f27e8b2d0afa20d9
Tufel Mansuri <tufelmansuri89@gmail.com>
2026-09-03 18:25:08 +0530

    dolve  problem

---
+++ my/src/api.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+axios.interceptors.request.use((config) => {
+  const token = localStorage.getItem("authToken");
+
+  if (token) {
+    config.headers = config.headers || {};
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("authToken");
+    }
+
+    return Promise.reject(error);
+  }
+);
--- my/src/main.jsx
+++ my/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './api'
 import App from './App.jsx'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
