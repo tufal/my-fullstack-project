89e07f6e3569dfd9a02d8464d39348bba1fff9cf
Tufel Mansuri <tufelmansuri89@gmail.com>
2026-09-03 17:12:55 +0530

    dolve backend problem

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -4,6 +4,8 @@ const auth = (req, res, next) => {
   try {
     const token = req.cookies?.token;
 
+    console.log("TOKEN:", token ? "Token received" : "No token");
+
     if (!token) {
       return res.status(401).json({
         message: "Please login to access this resource",
@@ -15,11 +17,15 @@ const auth = (req, res, next) => {
       process.env.JWT_SECRET
     );
 
+    console.log("DECODED USER:", decoded);
+
     req.user = decoded;
 
     next();
 
   } catch (err) {
+    console.error("AUTH ERROR:", err.message);
+
     return res.status(401).json({
       message: "Invalid or expired token",
     });
